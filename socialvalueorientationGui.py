5229b2cad82af4b12d7bda0f4ce5c661529e043c
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2017-06-13 22:32:43 +0200

    Creation de la fenetre de decision

--- socialvalueorientationGui.py
+++ socialvalueorientationGui.py
@@ -3,6 +3,7 @@
 This module contains the GUI
 """
 
+import sys
 import logging
 from PyQt4 import QtGui, QtCore
 from util.utili18n import le2mtrans
@@ -16,33 +17,68 @@ from client.cltgui.cltguiwidgets import WPeriod, WExplication, WSpinbox
 logger = logging.getLogger("le2m")
 
 
+class WMatrice(QtGui.QWidget):
+    def __init__(self, parent, matrice):
+        super(WMatrice, self).__init__()
+
+        layout = QtGui.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.widget = QtGui.QWidget()
+        layout.addWidget(self.widget)
+        # self.widget.setStyleSheet("background-color: white; "
+        #                           "border: 1px solid #D8D8D8;")
+        self.widget.setStyleSheet("border: 1px solid #D8D8D8;")
+
+        gridlayout = QtGui.QGridLayout()
+        self.widget.setLayout(gridlayout)
+
+        top, bottom = matrice
+
+        label_you = QtGui.QLabel(trans_SVO(u"You receive"))
+        gridlayout.addWidget(label_you, 0, 0)
+        label_other = QtGui.QLabel(trans_SVO(u"The other receives"))
+        gridlayout.addWidget(label_other, 2, 0)
+
+        self.radios_group = QtGui.QButtonGroup()
+        for i in range(len(top)):
+            gridlayout.addWidget(QtGui.QLabel(str(top[i])), 0, i+1)
+            radio = QtGui.QRadioButton()
+            self.radios_group.addButton(radio, i)
+            gridlayout.addWidget(radio, 1, i+1)
+            gridlayout.addWidget(QtGui.QLabel(str(bottom[i])), 2, i + 1)
+
+        gridlayout.addWidget(QtGui.QLabel(trans_SVO(u"You")), 0, len(top) + 1)
+        self.spinbox_you = QtGui.QSpinBox()
+        self.spinbox_you.setButtonSymbols(QtGui.QSpinBox.NoButtons)
+        gridlayout.addWidget(self.spinbox_you, 0, len(top) + 2)
+
+        gridlayout.addWidget(QtGui.QLabel(trans_SVO(u"Other")), 2, len(top) + 1)
+        self.spinbox_other = QtGui.QSpinBox()
+        self.spinbox_other.setButtonSymbols(QtGui.QSpinBox.NoButtons)
+        gridlayout.addWidget(self.spinbox_other, 2, len(top) + 2)
+
+        self.adjustSize()
+
+
 class GuiDecision(QtGui.QDialog):
-    def __init__(self, defered, automatique, parent, period, historique):
+    def __init__(self, defered, automatique, parent, matrice):
         super(GuiDecision, self).__init__(parent)
 
         # variables
         self._defered = defered
         self._automatique = automatique
-        self._historique = GuiHistorique(self, historique)
+        self.matrice = matrice
 
         layout = QtGui.QVBoxLayout(self)
 
-        # should be removed if one-shot game
-        wperiod = WPeriod(
-            period=period, ecran_historique=self._historique)
-        layout.addWidget(wperiod)
-
         wexplanation = WExplication(
             text=texts_SVO.get_text_explanation(),
-            size=(450, 80), parent=self)
+            size=(650, 250), parent=self)
         layout.addWidget(wexplanation)
 
-        self._wdecision = WSpinbox(
-            label=texts_SVO.get_text_label_decision(),
-            minimum=pms.DECISION_MIN, maximum=pms.DECISION_MAX,
-            interval=pms.DECISION_STEP, automatique=self._automatique,
-            parent=self)
-        layout.addWidget(self._wdecision)
+        self.wmatrice = WMatrice(self, self.matrice)
+        layout.addWidget(self.wmatrice)
 
         buttons = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok)
         buttons.accepted.connect(self._accept)
@@ -66,7 +102,24 @@ class GuiDecision(QtGui.QDialog):
             self._timer_automatique.stop()
         except AttributeError:
             pass
-        decision = self._wdecision.get_value()
+        try:
+            decision = self.wmatrice.radios_group.checkedId()
+            if decision == -1:
+                raise ValueError(trans_SVO(u"Please select a distribution"))
+            you = self.wmatrice.spinbox_you.value()
+            if you != self.matrice[0][decision]:
+                raise ValueError(trans_SVO(u"The value you wrote for 'you' "
+                                           u"doesn't correspond to the "
+                                           u"distribution you choose."))
+            other = self.wmatrice.spinbox_other.value()
+            if other != self.matrice[1][decision]:
+                raise ValueError(trans_SVO(u"The value you wrote for 'other' "
+                                           u"doesn't correspond to the "
+                                           u"distribution you choose."))
+        except ValueError as e:
+            QtGui.QMessageBox.warning(self, trans_SVO(u"Be careful"), e.message)
+            return
+
         if not self._automatique:
             confirmation = QtGui.QMessageBox.question(
                 self, le2mtrans(u"Confirmation"),
@@ -137,3 +190,10 @@ class DConfigure(QtGui.QDialog):
         pms.NOMBRE_PERIODES = self._spin_periods.value()
         pms.TAILLE_GROUPES = self._spin_groups.value()
         self.accept()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication([])
+    test_mat = GuiDecision(None, 0, None, pms.matrices_A[1])
+    test_mat.show()
+    sys.exit(app.exec_())
