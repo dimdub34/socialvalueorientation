b917bcabafee31b3674a111ba911b9bcf77be41f
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2017-06-13 15:44:13 +0200

    Suppression des parameters inutiles

--- socialvalueorientationParams.py
+++ socialvalueorientationParams.py
@@ -15,14 +15,6 @@ TREATMENTS_NAMES = {BASELINE: "Baseline"}
 # parameters -------------------------------------------------------------------
 TREATMENT = BASELINE
 TAUX_CONVERSION = 1
-NOMBRE_PERIODES = 0
-TAILLE_GROUPES = 0
-GROUPES_CHAQUE_PERIODE = False
 MONNAIE = u"None"
-PERIODE_ESSAI = False
 
-# DECISION
-DECISION_MIN = 0
-DECISION_MAX = 100
-DECISION_STEP = 1
 
