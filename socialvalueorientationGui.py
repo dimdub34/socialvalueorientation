1720664b7cb705a034ca117050f4ed8376c0dda3
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2017-06-14 11:02:21 +0200

    Ajout d'un affichage slider

--- socialvalueorientationGui.py
+++ socialvalueorientationGui.py
@@ -5,29 +5,27 @@ This module contains the GUI
 
 import sys
 import logging
+import random
 from PyQt4 import QtGui, QtCore
 from util.utili18n import le2mtrans
 import socialvalueorientationParams as pms
 from socialvalueorientationTexts import trans_SVO
 import socialvalueorientationTexts as texts_SVO
-from client.cltgui.cltguidialogs import GuiHistorique
-from client.cltgui.cltguiwidgets import WPeriod, WExplication, WSpinbox
+from client.cltgui.cltguiwidgets import WExplication
 
 
 logger = logging.getLogger("le2m")
 
 
 class WMatrice(QtGui.QWidget):
-    def __init__(self, parent, matrice):
-        super(WMatrice, self).__init__()
+    def __init__(self, parent, matrice, automatique):
+        super(WMatrice, self).__init__(parent)
 
         layout = QtGui.QVBoxLayout()
         self.setLayout(layout)
 
         self.widget = QtGui.QWidget()
         layout.addWidget(self.widget)
-        # self.widget.setStyleSheet("background-color: white; "
-        #                           "border: 1px solid #D8D8D8;")
         self.widget.setStyleSheet("border: 1px solid #D8D8D8;")
 
         gridlayout = QtGui.QGridLayout()
@@ -66,6 +64,85 @@ class WMatrice(QtGui.QWidget):
 
         self.adjustSize()
 
+        if automatique:
+            self.radios_group.button(random.randint(0, len(top)-1)).setChecked(True)
+            self.spinbox_you.setValue(top[self.radios_group.checkedId()])
+            self.spinbox_other.setValue(bottom[self.radios_group.checkedId()])
+
+
+class WSlide(QtGui.QWidget):
+    def __init__(self, parent, matrice, automatique):
+        super(WSlide, self).__init__(parent)
+
+        self.matrice = matrice
+        top, bottom = self.matrice
+        layout = QtGui.QVBoxLayout()
+        self.setLayout(layout)
+
+        wlcd = QtGui.QWidget()
+        wlcd.setStyleSheet("background-color: white;")
+        formlayout = QtGui.QFormLayout()
+        self.lcd_you = QtGui.QLCDNumber()
+        self.lcd_you.setFrameShape(QtGui.QLCDNumber.NoFrame)
+        self.lcd_you.setSegmentStyle(QtGui.QLCDNumber.Flat)
+        self.lcd_you.setStyleSheet("color: blue;")
+        self.lcd_you.setFixedHeight(20)
+        self.lcd_you.setFixedWidth(60)
+        formlayout.addRow(QtGui.QLabel(trans_SVO(u"You")), self.lcd_you)
+        self.lcd_other = QtGui.QLCDNumber()
+        self.lcd_other.setFrameShape(QtGui.QLCDNumber.NoFrame)
+        self.lcd_other.setSegmentStyle(QtGui.QLCDNumber.Flat)
+        self.lcd_other.setStyleSheet("color: blue;")
+        self.lcd_other.setFixedHeight(20)
+        self.lcd_other.setFixedWidth(60)
+        formlayout.addRow(QtGui.QLabel(trans_SVO(u"Other")), self.lcd_other)
+        wlcd.setLayout(formlayout)
+
+        hlayout = QtGui.QHBoxLayout()
+        hlayout.addSpacerItem(
+            QtGui.QSpacerItem(20, 5, QtGui.QSizePolicy.Expanding,
+                              QtGui.QSizePolicy.Minimum))
+        hlayout.addWidget(wlcd)
+        hlayout.addSpacerItem(
+            QtGui.QSpacerItem(20, 5, QtGui.QSizePolicy.Expanding,
+                              QtGui.QSizePolicy.Minimum))
+        layout.addLayout(hlayout)
+
+        layout.addSpacerItem(QtGui.QSpacerItem(5, 40, QtGui.QSizePolicy.Minimum,
+                              QtGui.QSizePolicy.Fixed))
+
+        # Sliders
+        gridlayout = QtGui.QGridLayout()
+        gridlayout.setHorizontalSpacing(30)
+        layout.addLayout(gridlayout)
+        label_you = QtGui.QLabel(trans_SVO(u"You receive"))
+        gridlayout.addWidget(label_you, 0, 0)
+        label_other = QtGui.QLabel(trans_SVO(u"The other receives"))
+        gridlayout.addWidget(label_other, 2, 0)
+        for i in range(len(top)):
+            gridlayout.addWidget(QtGui.QLabel(str(top[i])), 0, i+1)
+            gridlayout.addWidget(QtGui.QLabel(str(bottom[i])), 2, i + 1)
+
+        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider.setStyleSheet("QSlider{background: transparent}")
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(len(top)-1)
+        self.slider.setValue(0)
+        self.slider.setTickPosition(QtGui.QSlider.TicksBothSides)
+        self.slider.setTickInterval(1)
+        self.slider.valueChanged.connect(self.display)
+        gridlayout.addWidget(self.slider, 1, 1, 1, len(top))
+        self.display()
+
+        self.adjustSize()
+
+        if automatique:
+            self.slider.setValue(random.randint(0, len(top)-1))
+
+    def display(self):
+        self.lcd_you.display(self.matrice[0][self.slider.value()])
+        self.lcd_other.display(self.matrice[1][self.slider.value()])
+
 
 class GuiDecision(QtGui.QDialog):
     def __init__(self, defered, automatique, parent, matrice):
@@ -83,8 +160,12 @@ class GuiDecision(QtGui.QDialog):
             size=(650, 250), parent=self)
         layout.addWidget(wexplanation)
 
-        self.wmatrice = WMatrice(self, self.matrice)
-        layout.addWidget(self.wmatrice)
+        if pms.DISPLAY == pms.DISPLAY_SLIDER:
+            self.wslider = WSlide(self, self.matrice, self._automatique)
+            layout.addWidget(self.wslider)
+        elif pms.DISPLAY == pms.DISPLAY_RADIO:
+            self.wmatrice = WMatrice(self, self.matrice, self._automatique)
+            layout.addWidget(self.wmatrice)
 
         buttons = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok)
         buttons.accepted.connect(self._accept)
@@ -109,19 +190,24 @@ class GuiDecision(QtGui.QDialog):
         except AttributeError:
             pass
         try:
-            decision = self.wmatrice.radios_group.checkedId()
-            if decision == -1:
-                raise ValueError(trans_SVO(u"Please select a distribution"))
-            you = self.wmatrice.spinbox_you.value()
-            if you != self.matrice[0][decision]:
-                raise ValueError(trans_SVO(u"The value you wrote for 'you' "
-                                           u"doesn't correspond to the "
-                                           u"distribution you choose."))
-            other = self.wmatrice.spinbox_other.value()
-            if other != self.matrice[1][decision]:
-                raise ValueError(trans_SVO(u"The value you wrote for 'other' "
-                                           u"doesn't correspond to the "
-                                           u"distribution you choose."))
+
+            if pms.DISPLAY == pms.DISPLAY_SLIDER:
+                decision = self.wslider.slider.value()
+
+            elif pms.DISPLAY == pms.DISPLAY_RADIO:
+                decision = self.wmatrice.radios_group.checkedId()
+                if decision == -1:
+                    raise ValueError(trans_SVO(u"Please select a distribution"))
+                you = self.wmatrice.spinbox_you.value()
+                if you != self.matrice[0][decision]:
+                    raise ValueError(trans_SVO(u"The value you wrote for 'you' "
+                                               u"doesn't correspond to the "
+                                               u"distribution you choose."))
+                other = self.wmatrice.spinbox_other.value()
+                if other != self.matrice[1][decision]:
+                    raise ValueError(trans_SVO(u"The value you wrote for 'other' "
+                                               u"doesn't correspond to the "
+                                               u"distribution you choose."))
         except ValueError as e:
             QtGui.QMessageBox.warning(self, trans_SVO(u"Be careful"), e.message)
             return
@@ -151,34 +237,16 @@ class DConfigure(QtGui.QDialog):
         # treatment
         self._combo_treatment = QtGui.QComboBox()
         self._combo_treatment.addItems(
-            list(sorted(pms.TREATMENTS_NAMES.viewvalues())))
+            [v for k, v in sorted(pms.TREATMENTS_NAMES.items())])
         self._combo_treatment.setCurrentIndex(pms.TREATMENT)
         form.addRow(QtGui.QLabel(u"Traitement"), self._combo_treatment)
 
-        # nombre de périodes
-        self._spin_periods = QtGui.QSpinBox()
-        self._spin_periods.setMinimum(0)
-        self._spin_periods.setMaximum(100)
-        self._spin_periods.setSingleStep(1)
-        self._spin_periods.setValue(pms.NOMBRE_PERIODES)
-        self._spin_periods.setButtonSymbols(QtGui.QSpinBox.NoButtons)
-        self._spin_periods.setMaximumWidth(50)
-        form.addRow(QtGui.QLabel(u"Nombre de périodes"), self._spin_periods)
-
-        # periode essai
-        self._checkbox_essai = QtGui.QCheckBox()
-        self._checkbox_essai.setChecked(pms.PERIODE_ESSAI)
-        form.addRow(QtGui.QLabel(u"Période d'essai"), self._checkbox_essai)
-
-        # taille groupes
-        self._spin_groups = QtGui.QSpinBox()
-        self._spin_groups.setMinimum(0)
-        self._spin_groups.setMaximum(100)
-        self._spin_groups.setSingleStep(1)
-        self._spin_groups.setValue(pms.TAILLE_GROUPES)
-        self._spin_groups.setButtonSymbols(QtGui.QSpinBox.NoButtons)
-        self._spin_groups.setMaximumWidth(50)
-        form.addRow(QtGui.QLabel(u"Taille des groupes"), self._spin_groups)
+        # display
+        self._combo_display = QtGui.QComboBox()
+        self._combo_display.addItems(
+            [v for k, v in sorted(pms.DISPLAY_NAMES.items())])
+        self._combo_display.setCurrentIndex(pms.DISPLAY)
+        form.addRow(QtGui.QLabel(u"Affichage"), self._combo_display)
 
         button = QtGui.QDialogButtonBox(
             QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel)
@@ -192,14 +260,13 @@ class DConfigure(QtGui.QDialog):
 
     def _accept(self):
         pms.TREATMENT = self._combo_treatment.currentIndex()
-        pms.PERIODE_ESSAI = self._checkbox_essai.isChecked()
-        pms.NOMBRE_PERIODES = self._spin_periods.value()
-        pms.TAILLE_GROUPES = self._spin_groups.value()
+        pms.DISPLAY = self._combo_display.currentIndex()
         self.accept()
 
 
 if __name__ == "__main__":
     app = QtGui.QApplication([])
-    test_mat = GuiDecision(None, 0, None, pms.matrices_A[1])
+    # test_mat = WSlide(None, pms.matrices_A[2])
+    test_mat = GuiDecision(None, 1, None, pms.matrices_A[2])
     test_mat.show()
     sys.exit(app.exec_())
